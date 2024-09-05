62e387bfa29519bcc85a29460cf2cc2d45eb28ea
dchisholm125 <dchisholm125@gmail.com>
2024-09-05 13:20:26 -0400

    Changes to version 2.
    Changes to version 2.

--- components/ThistledownHeader.vue
+++ components/ThistledownHeader.vue
@@ -3,17 +3,17 @@
 </script>
 
 <template>
-    <div class="d-flex bg-thistle-tan w-100 m-0 p-0">
-        <img src="~/assets/thistledown-nav-logo.png" height="200" loading="lazy" />
-    </div>
-
-    <button class="btn btn-outline-success" height="20">Apply now!</button>
-
-    <div class="d-flex bg-thistle-tan-drk w-100 py-2 gap-2">
+    <div class="d-flex bg-thistle-tan-drk align-items-center justify-content-between w-100 py-2 gap-2">
         <div class="col-1"></div>
-        <a href="/"><span>Home</span></a>
-        <a href="#our-community"><span>Our Community</span></a>
-        <a href="/about"><span>About Us</span></a>
-        <a href="/something-else"><span>Something Else</span></a>
+        <div class="col d-flex ms-5 fs-5 fw-bold gap-2">
+            <a href="#about-us"><span>ABOUT US</span></a>
+            <a href="#our-mission"><span>OUR MISSION</span></a>
+            <a href="#our-home"><span>OUR HOME</span></a>
+            <a href="#our-community"><span>OUR COMMUNITY</span></a>
+            <a href="#our-approach"><span>OUR APPROACH</span></a>
+            <a href="#join-us"><span>JOIN US</span></a>
+        </div>
+        <button class="col-auto btn btn-success">Apply now!</button>
+        <div class="col-1"></div>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,99 +1,102 @@
 <template>
+
+    <div class="d-flex">
+        <img src="~/assets/thistledown-logo.svg" class="position-absolute h-0 w-0 border border-solid rounded img-fluid bg-opaque-white" 
+            alt="About Us" width="700" height="500" loading="lazy" style="top: 15%; left: 15%;">
+        <img class="img-fluid" src="~/assets/cate-bligh-rOjL1qsJ9vE-unsplash.jpg" />
+    </div>
+
     <ThistledownHeader />
 
-    <div class="container col-xxl-8 px-4 py-5">
-        <div class="row flex-lg-row-reverse align-items-center g-5 py-5 bg-heros">
-            <div class="col-10 col-sm-8 col-lg-6">
-                <img src="~/assets/thistledown-logo.svg" class="d-block mx-lg-auto border border-solid rounded img-fluid" alt="About Us" width="700" height="500" loading="lazy">
-            </div>
-            <div class="col-lg-6">
-                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">About Us</h1>
-                <p class="lead">Welcome to Thistledown Recovery Home, a dedicated sober living
-                    environment for men who are committed to transforming their lives and
-                    embracing sobriety. Our home is designed to provide a supportive and
-                    structured living space that fosters personal growth, accountability, and a
-                    sense of community among residents who are on a shared journey towards
-                    lasting recovery.</p>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-                </div>
+    <div id="about-us" class="d-flex banner-height m-0 p-0 align-items-center">
+        <div class="col-6 h-100">
+            <img src="~/assets/matheus-ferrero-TkrRvwxjb_8-unsplash.jpg" 
+            style="height: 100%; max-width: 100%; object-fit: cover;" alt="About Us" loading="lazy">
+        </div>
+        <div class="col-6 px-5">
+            <p class="display-6">CONWAY, NH</p>
+            <p class="display-4">Gateway to Recovery</p>
+            <p class="display-2">About Us</p>
+            <p class="lead">Welcome to Thistledown Recovery Home, a dedicated sober living
+                environment for men who are committed to transforming their lives and
+                embracing sobriety. Our home is designed to provide a supportive and
+                structured living space that fosters personal growth, accountability, and a
+                sense of community among residents who are on a shared journey towards
+                lasting recovery.</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
             </div>
         </div>
     </div>
 
-    <hr class="border border-solid"/>
-
-    <div class="container col-xxl-8 px-4 py-5">
-        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-lg-6">
-                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Our Mission</h1>
-                <p class="lead">At Thistledown Recovery Home, our mission is to offer a safe and stable
-                    residence for men who have made a conscious decision to stay sober. We
-                    understand that the path to recovery is unique for each individual, and we
-                    believe that a supportive home environment is crucial to maintaining
-                    sobriety. Our goal is to facilitate a space where residents can focus on their
-                    recovery without the distractions and triggers of their previous living
-                    situations.</p>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-                </div>
+    <div id="our-mission" class="d-flex banner-height m-0 p-0 align-items-center">
+        <div class="col-6 px-5">
+            <p class="display-6">CONWAY, NH</p>
+            <p class="display-4">Gateway to Recovery</p>
+            <p class="display-2">Our Mission</p>
+            <p class="lead">At Thistledown Recovery Home, our mission is to offer a safe and stable
+                residence for men who have made a conscious decision to stay sober. We
+                understand that the path to recovery is unique for each individual, and we
+                believe that a supportive home environment is crucial to maintaining
+                sobriety. Our goal is to facilitate a space where residents can focus on their
+                recovery without the distractions and triggers of their previous living
+                situations.</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
             </div>
-            <div class="col-10 col-sm-8 col-lg-6">
-                <img src="~/assets/thistledown-logo.svg" class="d-block mx-lg-auto border border-solid rounded img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
-            </div>
+        </div>
+        <div class="col-6 h-100">
+            <img src="~/assets/kalen-emsley-4mHZqgoZQu8-unsplash.jpg" 
+            style="height: 100%; max-width: 100%; object-fit: cover;" alt="About Us" loading="lazy">
         </div>
     </div>
 
-    <hr class="border border-solid"/>
-
-    <div class="container col-xxl-8 px-4 py-5">
-        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-10 col-sm-8 col-lg-6">
-                <img src="~/assets/thistledown-logo.svg" class="d-block mx-lg-auto border border-solid rounded img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
-            </div>
-            <div class="col-lg-6">
-                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Our Home</h1>
-                <p class="lead">Located in Conway, New Hampshire, our sober home is situated in a
+    <div id="our-home" class="d-flex banner-height m-0 p-0 align-items-center">
+        <div class="col-6 h-100">
+            <img src="~/assets/luke-stackpoole-eWqOgJ-lfiI-unsplash.jpg" 
+            style="height: 100%; max-width: 100%; object-fit: cover;" alt="Our Home" loading="lazy">
+        </div>
+        <div class="col-6 px-5">
+            <p class="display-6">CONWAY, NH</p>
+            <p class="display-4">Gateway to Recovery</p>
+            <p class="display-2">Our Home</p>
+            <p class="lead">Located in Conway, New Hampshire, our sober home is situated in a
                     peaceful and welcoming community. The residence features comfortable
                     living quarters, communal spaces for fellowship and relaxation, and
                     amenities that cater to the needs of our residents. We take pride in
                     maintaining a clean and orderly home to ensure a conducive atmosphere
                     for recovery.</p>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-                </div>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
             </div>
         </div>
     </div>
 
-    <hr class="border border-solid"/>
-
-    <div class="container col-xxl-8 px-4 py-5">
-        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-lg-6">
-                <h1 id="our-community" class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Our Community</h1>
-                <p class="lead">Thistledown is situated in a vibrant community, surrounded by a plethora of
+    <div id="our-community" class="d-flex banner-height m-0 p-0 align-items-center">
+        <div class="col-6 px-5">
+            <p class="display-6">CONWAY, NH</p>
+            <p class="display-4">Gateway to Recovery</p>
+            <p class="display-2">Our Community</p>
+            <p class="lead">Thistledown is situated in a vibrant community, surrounded by a plethora of
                     diverse businesses. Its convenient location offers residents the unique
                     advantage of being within walking distance or a short commute to a wide
                     range of employment opportunities. Residents are permitted to store
                     personal bicycles on property and a local taxi service is available for
                     transportation needs, ensuring everyone has easy access to reliable travel
                     to and from their place of employment.</p>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-                </div>
-            </div>
-            <div class="col-10 col-sm-8 col-lg-6">
-                <img src="~/assets/thistledown-logo.svg" class="d-block mx-lg-auto border border-solid rounded img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
             </div>
         </div>
+        <div class="col-6 h-100">
+            <img src="~/assets/johannes-plenio-RwHv7LgeC7s-unsplash.jpg" 
+            style="height: 100%; max-width: 100%; object-fit: cover;" alt="About Us" loading="lazy">
+        </div>
     </div>
-
-    <hr class="border border-solid"/>
 
     <div class="container col-xxl-8 px-4 py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
@@ -101,7 +104,7 @@
                 <img src="~/assets/thistledown-logo.svg" class="d-block mx-lg-auto border border-solid rounded img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
             </div>
             <div class="col-lg-6">
-                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Our Approach</h1>
+                <h1 id="our-approach" class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Our Approach</h1>
                 <p class="lead">While we do not offer treatment services, our sober home operates with
                     guidelines that encourage a sober lifestyle. We require that all residents
                     commit to staying drug and alcohol-free, actively participate in their
@@ -126,7 +129,7 @@
     <div class="container col-xxl-8 px-4 py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div class="col-lg-6">
-                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Join Us</h1>
+                <h1 id="join-us" class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Join Us</h1>
                 <p class="lead">If you or a loved one is seeking a men's sober living home that prioritizes a
                     sober and supportive living environment, Thistledown Recovery Home may
                     be the right fit. We invite you to learn more about our home and how we
@@ -146,6 +149,14 @@
 </template>
 
 <style>
+.banner-height {
+    height: 80vh;
+}
+
+.bg-opaque-white {
+    background-color: rgba(255, 255, 255, 0.75);
+}
+
 .bg-thistle-tan {
     background-color: white;
 }
