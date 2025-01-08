4e389553c2debffab60c04c9420338ca657bd572
Derek C <dchisholm125@gmail.com>
2025-01-07 23:33:03 -0500

    change from min to CSS file.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   css: [
-    "~/assets/bootstrap/dist/css/bootstrap.min.css"
+    "~/assets/bootstrap/dist/css/bootstrap.css"
   ],
 
   modules:  [
