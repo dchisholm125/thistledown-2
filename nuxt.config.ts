1a000d3a8b3280e4d4920e4b84b90914859ae584
Derek C <dchisholm125@gmail.com>
2025-01-24 10:37:21 -0500

    Change vite folders

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   build: {
@@ -35,5 +38,22 @@ export default defineNuxtConfig({
       }
   }],  
   ],
-  routeRules: { "/": { prerender: true, }, }
+  routeRules: { "/": { prerender: true, }, },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+          sanitizeFileName(name) {
+            const match = DRIVE_LETTER_REGEX.exec(name);
+            const driveLetter = match ? match[0] : "";
+            return (
+              driveLetter +
+              name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+            );
+          },
+        },
+      },
+    },
+  },
 })
