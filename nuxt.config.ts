4fb0200aa70d8c77d5dba8d6a6aa972d11c23ae2
Derek C <dchisholm125@gmail.com>
2025-01-24 10:33:47 -0500

    Change dir name for jekyll build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    publicPath: '/thistledown/'
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
